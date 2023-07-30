388f0002dc04ff064b167b08490c36ab0efe405d
Harsh Kumar Raghav <raghavharsh068@gmail.com>
2023-07-30 17:46:53 +0530

    Worked on loading

--- src/app/User/[username]/page.tsx
+++ src/app/User/[username]/page.tsx
@@ -1,23 +1,17 @@
 import UserDetailProvider from "@/Providers/UserDetailProvider";
-import PageLoader from "@/components/Loader/PageLoader";
 import Header from "@/components/Navigation/Header";
 import UserProfile from "@/components/UserProfile/UserProfile";
-import React, { Suspense } from "react";
 interface Props {
   params: {
     username: string;
   };
 }
-
 const page = ({ params }: Props) => {
   const { username } = params;
-
   return (
     <UserDetailProvider>
       <Header />
-      <Suspense fallback={<PageLoader />}>
-        <UserProfile UserName={username} />
-      </Suspense>
+      <UserProfile UserName={username} />
     </UserDetailProvider>
   );
 };
