1fe464e89d567edd188a77f97ef872b1113db038
Harsh Kumar Raghav <raghavharsh068@gmail.com>
2023-07-30 19:46:47 +0530

    updated useInfiniteFetch  custom hook

--- src/hooks/useInfiniteFetch.ts
+++ src/hooks/useInfiniteFetch.ts
@@ -17,24 +17,17 @@ const fetchPost = async (page: number) => {
   );
   return data as UnsplashPhotoData[];
 };
-/**
- * The `useInfiniteFetch` function is a custom hook that uses the `useInfiniteQuery` hook from the
- * `react-query` library to fetch posts in an infinite scroll manner.
- * @returns The `useInfiniteFetch` function is returning the result of calling the `useInfiniteQuery`
- * hook.
- */
+
 export const useInfiniteFetch = () => {
-  return useInfiniteQuery(
-    ["posts"],
-    ({ pageParam = 1 }) => fetchPost(pageParam),
-    {
-      getNextPageParam: (_, allPages) => {
-        const maxPage = 20000 / 10;
-        if (allPages.length < maxPage) {
-          return allPages.length + 1;
-        }
-        return undefined;
-      },
-    }
-  );
+  return useInfiniteQuery({
+    queryKey: ["posts"],
+    queryFn: ({ pageParam = 1 }) => fetchPost(pageParam),
+    getNextPageParam: (_, allPages) => {
+      const maxPage = 20000 / 10;
+      if (allPages.length < maxPage) {
+        return allPages.length + 1;
+      }
+      return undefined;
+    },
+  });
 };
